dbbeee976bd39f8177fee2f2484552f3e81c2eec
ItsKalfox <nmwijethunga@students.nsbm.ac.lk>
2026-04-13 13:24:06 +0530

    feat(database): configure entity relationships

--- backend/PAS.API/Data/PASDbContext.cs
+++ backend/PAS.API/Data/PASDbContext.cs
@@ -10,5 +10,15 @@ public class PASDbContext : DbContext
     {
     }
 
+    public DbSet<User> Users { get; set; }
     public DbSet<Student> Students { get; set; }
+    public DbSet<Supervisor> Supervisors { get; set; }
+    public DbSet<ModuleLeader> ModuleLeaders { get; set; }
+    public DbSet<ResearchArea> ResearchAreas { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Coursework> Courseworks { get; set; }
+    public DbSet<CourseworkProject> CourseworkProjects { get; set; }
+    public DbSet<Match> Matches { get; set; }
+    public DbSet<Interest> Interests { get; set; }
 }
